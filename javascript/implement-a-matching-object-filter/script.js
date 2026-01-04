a108b6fccfe8362b2789bc1e53e8619e1578f2c7
Jose Angel Garcia <jgarciarodriguez53@gmail.com>
2026-01-03 20:19:13 -0500

    final code

--- javascript/implement-a-matching-object-filter/script.js
+++ javascript/implement-a-matching-object-filter/script.js
@@ -1,1 +1,7 @@
-console.log("HOLA"); 
+function whatIsInAName(arrObjs, srcObj) {
+  const srcKeys = Object.keys(srcObj);
+
+  return arrObjs.filter(obj => 
+    srcKeys.every(key => obj.hasOwnProperty(key) && obj[key] === srcObj[key])
+  );
+}
