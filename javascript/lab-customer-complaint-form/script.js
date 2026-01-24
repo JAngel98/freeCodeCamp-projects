30e131b4c6121a32f6d6f81e7134f74466b48a73
Jose Angel Garcia <jgarciarodriguez53@gmail.com>
2026-01-23 21:00:28 -0500

    lab-customer-complaint-form [solution]

--- javascript/lab-customer-complaint-form/script.js
+++ javascript/lab-customer-complaint-form/script.js
@@ -1,17 +1,28 @@
-const redColor = '#EE0000';
-const greenColor = '#00AA00';
+const formFields = {
+    'full-name': document.getElementById('full-name'),
+    'email': document.getElementById('email'),
+    'order-no': document.getElementById('order-no'),
+    'product-code': document.getElementById('product-code'),
+    'quantity': document.getElementById('quantity'),
+    'complaints-group': document.getElementById('complaints-group'),
+    'complaint-description': document.getElementById('complaint-description'),
+    'solutions-group': document.getElementById('solutions-group'),
+    'solution-description': document.getElementById('solution-description')
+};
 
 function validateForm() {
+    const checkBoxs = document.querySelectorAll('#complaints-group > div > input');
+    const radioBtns = document.querySelectorAll('#solutions-group > input');
     const inputsObj = {
-        'full-name': false,
-        'email': false,
-        'order-no': false,
-        'product-code': false,
-        'quantity': false,
-        'complaints-group': false,
-        'complaint-description': false,
-        'solutions-group': false,
-        'solution-description': false
+        'full-name': true,
+        'email': true,
+        'order-no': true,
+        'product-code': true,
+        'quantity': true,
+        'complaints-group': true,
+        'complaint-description': true,
+        'solutions-group': true,
+        'solution-description': true
     };
 
     for (const input in inputsObj) {
@@ -19,53 +30,74 @@ function validateForm() {
         if (!inputElm) return null;
 
         if (input === 'full-name') {
-            if (inputElm.value === '') {
-                inputElm.style.borderColor = redColor;
-                inputsObj[input] = false;
-            } else {
-                inputElm.style.borderColor = greenColor;
-                inputsObj[input] = true;
-            }
+            if (inputElm.value === '') inputsObj[input] = false;
         }
         if (input === 'email') {
-            if (!inputElm.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
-                inputElm.style.borderColor = redColor;
-                inputsObj[input] = false;
-            } else {
-                inputElm.style.borderColor = greenColor;
-                inputsObj[input] = true;
-            }
+            if (!inputElm.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) inputsObj[input] = false;
         }
         if (input === 'order-no') {
-            if (!inputElm.value.match(/^2024[0-9]{6}/)) {
-                inputElm.style.borderColor = redColor;
-                inputsObj[input] = false;
-            } else {
-                inputElm.style.borderColor = greenColor;
-                inputsObj[input] = true;
-            }
+            if (!inputElm.value.match(/^2024[0-9]{6}/)) inputsObj[input] = false;
         }
         if (input === 'product-code') {
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Quantifier
-            if (!inputElm.value.match(/^2024[0-9]{6}/)) {
-                inputElm.style.borderColor = redColor;
-                inputsObj[input] = false;
-            } else {
-                inputElm.style.borderColor = greenColor;
-                inputsObj[input] = true;
-            }
+            if (!inputElm.value.match(/^[A-Z]{2}\d{2}-[A-Z]\d{3}-[A-Z]{2}\d$/i)) inputsObj[input] = false;
         }
-        //console.log(inputElm);
+        if (input === 'quantity') {
+            if (Number(inputElm.value) < 1) inputsObj[input] = false;
+        }
+        if (input === 'complaints-group') {
+            const isChecked = Array.from(checkBoxs).some(i => i.checked);
 
+            if (!isChecked) inputsObj[input] = false;
+        }
+        if (input === 'complaint-description') {
+            const otherCheck = Array.from(checkBoxs).filter(i => i.id === 'other-complaint')[0];
+
+            if (otherCheck.checked && inputElm.value.length < 20) inputsObj[input] = false;
+        }
+        if (input === 'solutions-group') {
+            const isChecked = Array.from(radioBtns).some(i => i.checked);
+
+            if (!isChecked) inputsObj[input] = false;
+        }
+        if (input === 'solution-description') {
+            const otherCheck = Array.from(radioBtns).filter(i => i.id === 'other-solution')[0];
+
+            if (otherCheck.checked && inputElm.value.length < 20) inputsObj[input] = false;
+        }
     }
-
-    console.log(inputsObj);
-    
 
     return inputsObj;
 }
 
+function isValid(obj) {
+    let isValidForSubmit = Object.values(obj).every(key => key === true);
+
+    return isValidForSubmit;
+}
+
+Object.keys(formFields).forEach((key) => {
+    document.getElementById(key).addEventListener("change", () => {
+        const isAValidInput = validateForm()[key];
+        if (isAValidInput) {
+            document.getElementById(key).style.borderColor = "green";
+        } else {
+            document.getElementById(key).style.borderColor = "red";
+        }
+    });
+})
+
+
 document.getElementById("form").addEventListener("submit", (e) => {
     e.preventDefault();
-    validateForm();
+    const validationObj = validateForm();
+
+    if (!isValid(validationObj)) {
+        Object.keys(validationObj).forEach((key) => {
+            if (validationObj[key]) {
+                document.getElementById(key).style.borderColor = "green";
+            } else {
+                document.getElementById(key).style.borderColor = "red";
+            }
+        })
+    }
 })
